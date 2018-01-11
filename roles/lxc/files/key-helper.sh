d89add215ee576b1e69e390249cfd0d9129c74be
Adam Vlasak <adam.vlasak@gmail.com>
2018-01-11 21:07:50 +0100

    removing lxc stuff

--- roles/lxc/files/key-helper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for CONTAINER in $(ls -1 /var/lib/lxc); do
-	echo "Copying id_rsa.pub to ${CONTAINER}"
-	mkdir -p "/var/lib/lxc/${CONTAINER}/rootfs/root/.ssh" && \
-	cp ~/.ssh/id_rsa.pub "/var/lib/lxc/${CONTAINER}/rootfs/root/.ssh/authorized_keys" && \
-	chown 0600 "/var/lib/lxc/${CONTAINER}/rootfs/root/.ssh" && \
-	chown 0400 "/var/lib/lxc/${CONTAINER}/rootfs/root/.ssh/authorized_keys";
-done
