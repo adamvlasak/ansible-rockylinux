39c4d8f6431be38b85f7fd08865adc9676ca262b
Adam Vlasak <adam.vlasak@gmail.com>
2018-06-04 17:09:20 +0200

    UPDATE: small tweaks to shopt

--- roles/user/templates/vars.sh
+++ roles/user/templates/vars.sh
@@ -9,6 +9,13 @@ export HISTCONTROL=ignoreboth
 export NCURSES_NO_UTF8_ACS=1
 
 # append to the history file, don't overwrite it
-shopt -s histappend
+shopt -s -q histappend
 
-shopt -s checkwinsize
+# Make sure display get updated when terminal window get resized
+shopt -s -q checkwinsize
+
+# Correct dir spellings
+shopt -s -q cdspell
+
+# Turn on the extended pattern matching features
+shopt -s -q extglob
