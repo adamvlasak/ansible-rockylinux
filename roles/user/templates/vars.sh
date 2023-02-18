59c5bc29f8aa71db890f99885a1bd50e09517e3a
Adam Vlasák <adam.vlasak@gmail.com>
2023-02-18 20:05:33 +0100

    update vimrc and vars.sh

--- roles/user/templates/vars.sh
+++ roles/user/templates/vars.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-export LANG=en_US.UTF-8
-export EDITOR=vim
-export BROWSER=w3m
-export TERM=xterm-256color
-export HISTSIZE=1000
-export HISTFILESIZE=1000
-export HISTCONTROL=ignoreboth
-export NCURSES_NO_UTF8_ACS=1
+export LANG="en_US.UTF-8"
+export EDITOR="vim"
+export HISTSIZE="1000"
+export HISTFILESIZE="1000"
+export HISTCONTROL="ignoreboth"
 
 # append to the history file, don't overwrite it
 shopt -s -q histappend
