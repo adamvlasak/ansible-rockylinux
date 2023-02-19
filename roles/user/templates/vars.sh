8c4da14c0c7a7a1a6516f53f5ab45cefdac356b0
Adam Vlasák <adam.vlasak@gmail.com>
2023-02-19 18:53:20 +0100

    export HISTCONTROL="ignoredups:erasedups"

--- roles/user/templates/vars.sh
+++ roles/user/templates/vars.sh
@@ -4,7 +4,7 @@ export LANG="en_US.UTF-8"
 export EDITOR="vim"
 export HISTSIZE="1000"
 export HISTFILESIZE="1000"
-export HISTCONTROL="ignoreboth"
+export HISTCONTROL="ignoredups:erasedups"
 
 # append to the history file, don't overwrite it
 shopt -s -q histappend
