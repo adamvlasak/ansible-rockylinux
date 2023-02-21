47e202e598e84b88849aafc37402182ba532e18b
Adam Vlasák <adam.vlasak@gmail.com>
2023-02-21 11:32:07 +0100

    ssh-agent as user service

--- roles/user/templates/vars.sh
+++ roles/user/templates/vars.sh
@@ -5,6 +5,7 @@ export EDITOR="vim"
 export HISTSIZE="1000"
 export HISTFILESIZE="1000"
 export HISTCONTROL="ignoredups:erasedups"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 
 # append to the history file, don't overwrite it
 shopt -s -q histappend
